c2ba2c86a8d52328bbe4b7563833f7a450ecb598
AMIEKHAME EMMANUEL OSHONE <134517138+Amiekhame@users.noreply.github.com>
2024-07-02 12:15:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("name").textContent = "Name: Amiekhame Emmanuel";
+document.getElementById("username").textContent = "Username: Amiekhame";
+document.getElementById("email").textContent = "Email: oshone.com";
